41e28d8f73868eff82d8370248fa70ed198eaf7c
Ashok <marannan@wisc.edu>
2015-12-03 13:29:27 -0600

    git_readme

---
+++ Docs/git_readme.txt
@@ -0,0 +1,6 @@
+git init
+git add * 
+git commit -a -m "comment"
+git remote add repo_name git url
+git push -f(force) -u(first time only) repo_name <dest_branchweb
+> mostly master
--- Scripts/git_update.sh
+++ Scripts/git_update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /media/ashokmarannan/My_Drive/Ashok/Personal/OneDrive/Documents/Github
+cd /media/ashokmarannan/My_Drive/Ashok/OneDrive/Github
 git add *
 git commit -m $1
-git push origin master
+git push repo_1 master
