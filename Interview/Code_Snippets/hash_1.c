9324dfef14a8b144bc84cc8eebbd67b0385683af
Ashok <marannan@wisc.edu>
2016-01-07 15:05:30 -0600

    clean_up_1

---
+++ Interview/Code_Snippets/hash_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int hash_fun(int a, int b)
+{
+	return a+b;
+}
+
+int main(int argc, char *argv[])
+{
+	char *str;
+	fgets(stdin,str,);
+
+	return 0;
+}
---
+++ Interview/Code_Snippets/hash_table_password.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int hash_table[256] = {0};
+char str[256];
+
+typedef struct lnkd_list
+{
+	int hash_val;
+	char *user_name;
+	char *password;
+	struct lnkd_list *next_ptr;
+	
+}linked_list;
+
+int put(int hash_val)
+{
+	sprintf(str,"hash_val: %d\n",hash_val);
+	debug(str);
+
+	hash_table[hash_val] = hash_table[hash_val] + 1;
+	sprintf(str,"hash_table[%d]: %d\n",hash_val, hash_table[hash_val]);
+	debug(str);
+
+	if(hash_table[hash_val] > 1)
+		return -1;
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+
+	int i = 0;
+	int str_len = 0;
+	int hash_val;
+
+	for(i = 0; i < strlen(argv[1]); i++)
+	{
+		hash_val = argv[1][i] % 256 ;
+		if(put(hash_val) != 0)
+		{
+			printf("no\n");
+			exit(0);
+		}
+	}
+
+	printf("yes\n");
+	return 0;
+}
