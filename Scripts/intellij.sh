a843d793c3317c046fb256d877856bc4858ec893
marannan <marannan@wisc.edu>
2016-01-17 21:12:48 -0600

    update_1

---
+++ Scripts/intellij.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /media/ashok/My_Drive/Downloads/idea-IC-143.1184.17/bin/
+./idea.sh
---
+++ Temp/Hello.java
@@ -0,0 +1,8 @@
+public class Hello
+{
+	public static void main(String [] s)
+	{
+		System.out.println("Kirthanna has a juicy ass!!");
+	}
+}
+
