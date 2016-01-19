20957a43ed6d7229d35b26b208238eb25080f547
marannan <marannan@wisc.edu>
2016-01-19 13:24:42 -0600

    code_snippets

---
+++ Interview/Code_Snippets/is_palin.py
@@ -0,0 +1,44 @@
+import sys
+import os
+
+
+
+
+def is_palin(string):
+    
+    if len(string) == 0:
+        return False
+    
+    i = 0
+    j = len(string) - 1
+    
+    #print string[2]
+    
+    while i < j:
+        if str(string[i]).isalpha()== False:
+            i = i + 1;
+            continue
+        if str(string[j]).isalpha() == False:
+            j = j - 1;        
+            continue
+
+        if string[i] != string[j]:
+            return False
+        i = i + 1
+        j = j - 1
+        
+    return True
+
+
+def main():
+    string = sys.argv[1]
+    
+    if is_palin(string) == True:
+        print "yes"
+    else:
+        "no"
+    
+    
+    return
+if __name__ == "__main__":
+    main()
---
+++ Interview/Code_Snippets/string_permutations.py
@@ -0,0 +1,49 @@
+import sys
+import os
+
+b = [0,0,0]
+c = [0,1]
+
+def swap(a,l,r):
+    t = a[l]
+    a[l] = a[r]
+    a[r] = t
+    return a
+ 
+def toString(List):
+    return ''.join(List)
+ 
+# Function to print permutations of string
+# This function takes three parameters:
+# 1. String
+# 2. Starting index of the string
+# 3. Ending index of the string.
+def permute(a, l, r):
+    if l==r:
+        print toString(a)
+    else:
+        for i in xrange(l,r+1):
+            a = swap(a,l,i)
+            permute(a, l+1, r)
+            a = swap(a,l,i) # backtrack
+            
+            
+def perm(n1, n2):
+    global c, b
+    if n1 <= 0:
+        print b
+    else:
+        for l in c:
+            b[n1-1] = l
+            perm(n1-1, n2)
+        #b[n1-1] = 1
+        #perm(n1-1, n2)
+    
+ 
+if __name__ == "__main__":# Driver program to test the above function
+    string = "ABC"
+    n = len(string)
+    a = list(string)
+    permute(a, 0, n-1)
+
+    perm(3, 3)
