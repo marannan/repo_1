7d424de6802da706953bb1a8b8baea161754b094
marannan <marannan@wisc.edu>
2016-03-09 20:51:15 -0600

    adding_ps_repos_to_my_git

--- Scripts/git_update.sh
+++ Scripts/git_update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cd ..
+cd /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1
+echo "cding to /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1"
 git add *
 git commit -m $1
 git push
