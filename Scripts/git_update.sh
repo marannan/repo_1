e174eeefcb7558e966745b813df47182a3dd788f
marannan <marannan@wisc.edu>
2016-03-09 22:32:43 -0600

    git_scripts_1

--- Scripts/git_update.sh
+++ Scripts/git_update.sh
@@ -2,7 +2,7 @@
 cd /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1
 echo "---------------------"
 echo "syncing github at "
-echo pwd
+pwd
 echo "---------------------"
 git add *
 git commit -m $1
