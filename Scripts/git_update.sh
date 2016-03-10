1797d55667bb6ca6ab936937d240fb82f9d51303
marannan <marannan@wisc.edu>
2016-03-09 21:32:05 -0600

    script_changes_new_2

--- Scripts/git_update.sh
+++ Scripts/git_update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1
+pwd
 echo "-----------------------------------------------------------------------------------"
 echo "| syncing github at /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1 |"
 echo "-----------------------------------------------------------------------------------"
