e113bfe5c9c80bcbb3057fe20455e5324064ffde
marannan <marannan@wisc.edu>
2016-03-09 21:27:54 -0600

    script_changes

--- Scripts/git_update.sh
+++ Scripts/git_update.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 cd /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1
+echo "-------------------------------------------------------------------------------"
 echo "syncing github at /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1"
+echo "-------------------------------------------------------------------------------"
 git add *
 git commit -m $1
 git push
+echo "----------"
 echo "completed!"
+echo "----------"
--- Scripts/google_drive_sync.sh
+++ Scripts/google_drive_sync.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd /media/ashok/My_Drive/Ashok/Google_Drive_2/
+echo "-------------------------------------------------------------------"
 echo "syncing google drive at /media/ashok/My_Drive/Ashok/Google_Drive_2/"
+echo "-------------------------------------------------------------------"
 grive sync
+echo "----------"
 echo "completed!"
-
+echo "----------"
