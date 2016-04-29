0931d19b2fc0d1c9daf39767c2ad181c7c1e3e2f
marannan <marannan@wisc.edu>
2016-04-29 15:52:51 -0500

    Fri Apr 29 15:52:50 CDT 2016

--- Scripts/git_update.sh
+++ Scripts/git_update.sh
@@ -7,7 +7,7 @@ echo "time         : $(date)" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs
 echo "status       : started" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
 git add * >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
 git commit -a -m "$(date)" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
-git push origin master >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log 2>&1 &
+git push origin master >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
 echo "status 	   : completed" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
 echo "time         : $(date)" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
 echo "-----------------------------------------------------------" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
