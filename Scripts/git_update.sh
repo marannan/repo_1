881f74c60fd04f0489bdb1d397ed011d338c3919
marannan <marannan@wisc.edu>
2016-04-29 17:08:01 -0500

    Fri Apr 29 17:08:01 CDT 2016

--- Scripts/git_update.sh
+++ Scripts/git_update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 source /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/ssh-agent.cf
-echo "----------------------------------------------------------" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
-cd /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1 >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
-echo "git          : sync" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
-echo "directory    : $(pwd)" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
-echo "time         : $(date)" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
-echo "status       : started" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
-git add * >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
-git commit -a -m "$(date)" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
-git push origin master >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log 2>&1 &
-echo "status 	   : completed" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
-echo "time         : $(date)" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
-echo "-----------------------------------------------------------" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+echo "----------------------------------------------------------"
+cd /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1
+echo "git          : sync"
+echo "directory    : $(pwd)"
+echo "time         : $(date)"
+echo "status       : started"
+git add *
+git commit -a -m "$(date)"
+git push origin master
+echo "status 	   : completed"
+echo "time         : $(date)"
+echo "-----------------------------------------------------------"
