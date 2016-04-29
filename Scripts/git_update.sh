a8bda90d1dca2892f9dca26e94dca84e3d01e5d9
marannan <marannan@wisc.edu>
2016-04-29 15:42:03 -0500

    Fri Apr 29 15:42:03 CDT 2016

--- Scripts/git_update.sh
+++ Scripts/git_update.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-echo "----------------------------------------------------------"
-cd /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1
-echo "git          : sync"
-echo "directory    : $(pwd)"
-echo "time         : $(date)"
-echo "status       : started"
-git add *
-git commit -a -m "$(date)"
-git push
-echo "status 	   : completed"
-echo "time         : $(date)"
-echo "-----------------------------------------------------------"
+echo "----------------------------------------------------------" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+cd /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1 >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+echo "git          : sync" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+echo "directory    : $(pwd)" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+echo "time         : $(date)" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+echo "status       : started" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+git add * >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+git commit -a -m "$(date)" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+git push >> cd /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+echo "status 	   : completed" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+echo "time         : $(date)" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
+echo "-----------------------------------------------------------" >> /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1/Scripts/Logs/git_sync.log
