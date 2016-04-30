7f5c95c2f11505c0af7b7af79d48a03d55b41d91
marannan <marannan@wisc.edu>
2016-04-29 22:31:01 -0500

    Fri Apr 29 22:31:01 CDT 2016

--- Scripts/git_update.sh
+++ Scripts/git_update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. ~/.keychain/`/bin/hostname`-sh
 echo "----------------------------------------------------------"
 cd /media/ashok/My_Drive/Ashok/Google_Drive_2/Docs/Github/repo_1
 echo "git          : sync"
@@ -7,7 +8,7 @@ echo "time         : $(date)"
 echo "status       : started"
 git add *
 git commit -a -m "$(date)"
-git push https://marannan:0x!Gorange@github.com/marannan/repo_1.git master
+git push origin master
 echo "status 	     : completed"
 echo "time         : $(date)"
 echo "-----------------------------------------------------------"
